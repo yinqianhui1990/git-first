a645a5d36d913020cd48cda677bb41d36870c5b5
unknown <ThinkPad@yinqianhui.lachesis-mh.com>
2016-12-13 14:59:29 +0800

    Initial commit

---
+++ first/src/com/yqh.first/HelloGit.java
@@ -0,0 +1,10 @@
+package com.yqh.first;
+
+/**
+ * Created by ThinkPad on 2016/12/13.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git !!!");
+    }
+}
